d0e6b45f2e623b17919817604d8dbb6095b4047b
Ahmet Sertaç Dinçer <srtc.dncr@outlook.com>
2020-12-21 23:55:00 +0300

    initial commit main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+a1 = input()
+a2 = input()
+a3 = input()
+a4 = input()
+a5 = input()
+
+print(f"The type of a1 is {type(a1)} and value of a1 is {a1}")
+print(f"The type of a2 is {type(a2)} and value of a1 is {a2}")
+print("The type of a3 is {} and value of a1 is {}".format(type(a3), a3))
+print("The type of a4 is {} and value of a1 is {}".format(type(a4), a4))
+print("The type of a5 is {} and value of a1 is {}".format(type(a5), a5))
